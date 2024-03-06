a1ca28b8fed11d18bcdad27e556220aa84972c3f
MokaSrinu <srinumoka75@gmail.com>
2024-03-06 21:57:28 +0530

    feat(community): community feature added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -20,7 +20,7 @@ async function Home({
 
   const result = await fetchPosts(
     searchParams.page ? +searchParams.page : 1,
-    30
+    10
   );
 
   return (
@@ -28,11 +28,11 @@ async function Home({
       <h1 className='head-text text-left'>Home</h1>
 
       <section className='mt-9 flex flex-col gap-10'>
-        {result.posts.length === 0 ? (
+        {result?.posts?.length === 0 ? (
           <p className='no-result'>No threads found</p>
         ) : (
           <>
-            {result.posts.map((post) => (
+            {result?.posts?.map((post) => (
               <ThreadCard
                 key={post._id}
                 id={post._id}
